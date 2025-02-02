298431bdfa6a6a61033569e9bffd49649b6eecda
nickkhalow <nickhalow@gmail.com>
2025-02-02 20:10:12 +0200

    fix consistency

--- Assets/Script/MovePlayer.cs
+++ Assets/Script/MovePlayer.cs
@@ -1,75 +1,75 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class MovePlayer : MonoBehaviour
 {
-
-    public Rigidbody2D rb;
-    public Vector2 moveVector;
-    public float speed = 2f;
-
-    void Start()
-    {
-        rb =GetComponent <Rigidbody2D>();
-    }
-
-  
-    void Update()
-    {
-        walk();
-        Jump();
-        ChekDownTrig();
-    }
-
-
-    void walk()
-    {
-        moveVector.x = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(moveVector.x * speed, rb.linearVelocity.y);
-    }
+    [SerializeField]
+    private Rigidbody2D rb;
+    [SerializeField]
+    private Vector2 moveVector;
+    [SerializeField]
+    private float speed = 2f;
 
     public float jumpForse = 300f;
-    private int jumpCount = 0;
     public int maxJumpValeu = 2;
-    private bool jumpControl;
-    private int jumpItaration = 0;
-    public int jumpValueItatation = 60;
-
-
-    void Jump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && (OnDown || ++jumpCout < maxJumpValeu))
-        {
-            rb.AddForce(Vector2.up * jumpForse);
-        }
-
-        if (OnDown) {jumpCount = 0;}
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            if(OnDown) {jumpControl =true;}
-        }
-        else {jumpControl = false;}
-
-        if (jumpControl)
-        {
-            if(jumpItaration++ < jumpValueItatation )
-            {
-                rb.AddForce(Vector2.up * jumpForse /jumpItaration );
-            }
-            else {jumpItaration = 0;}
-        }
-
-    }
-
-
+    [FormerlySerializedAs("jumpValueIteration")] [FormerlySerializedAs("jumpValueItatation")]
+    public int maxAllowedJumpIteration = 60;
 
     public bool OnDown;
     public Transform DownTrig;
     public float checkRadius = 0.5f;
     public LayerMask paltformTrig;
-    private int jumpCout;
 
-    void ChekDownTrig()
+
+    [Header("Debug")]
+    [SerializeField]
+    private int jumpCount = 0;
+    [FormerlySerializedAs("jumpItaration")] [SerializeField]
+    private int jumpIteration = 0;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Walk();
+        Jump();
+        CheckDownTrig();
+    }
+
+    private void Walk()
+    {
+        moveVector.x = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveVector.x * speed, rb.velocity.y);
+    }
+
+    private void CheckDownTrig()
     {
         OnDown = Physics2D.OverlapCircle(DownTrig.position, checkRadius, paltformTrig);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && (OnDown || jumpCount < maxJumpValeu))
+        {
+            jumpCount++;
+            rb.AddForce(Vector2.up * jumpForse);
+        }
+
+        if (OnDown)
+        {
+            jumpCount = 0;
+            jumpIteration = 0;
+        }
+
+        var jumpControl = Input.GetKeyDown(KeyCode.Space) && OnDown;
+
+        if (jumpControl && jumpIteration < maxAllowedJumpIteration)
+        {
+            jumpIteration++;
+            rb.AddForce(Vector2.up * (jumpForse / jumpIteration));
+        }
     }
 }
